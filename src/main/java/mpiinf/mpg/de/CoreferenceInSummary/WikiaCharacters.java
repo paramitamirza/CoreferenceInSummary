7193465fa8504f23b09e348b91e24aeb0aef4a66
paramitamirza <paramita@lap-15-61.mpi-inf.mpg.de>
2017-08-30 15:42:43 +0200

    wikia

--- src/main/java/mpiinf/mpg/de/CoreferenceInSummary/WikiaCharacters.java
+++ src/main/java/mpiinf/mpg/de/CoreferenceInSummary/WikiaCharacters.java
@@ -1,8 +1,11 @@
 package mpiinf.mpg.de.CoreferenceInSummary;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.json.*;
@@ -11,30 +14,52 @@ import edu.stanford.nlp.coref.data.Dictionaries.Gender;
 
 public class WikiaCharacters {
 	
-	public String listOfCharacters(String wikiaUrl, String url, Gender gender) {
-		URL oracle = new URL("http://harrypotter.wikia.com/api/v1/Articles/List?expand=1&category=Females&limit=2000");
+	public String listOfCharacters(String wikiaUrl, String url, Gender gender) throws IOException {
         BufferedReader in = new BufferedReader(
-        new InputStreamReader(oracle.openStream()));
+        new InputStreamReader(new URL(url).openStream()));
 
         String input = "", inputLine;
         while ((inputLine = in.readLine()) != null)
             input += inputLine + "\n";
         in.close();
+        
+        StringBuilder output = new StringBuilder();
+        
+        String gen = "N";
+        if (gender == Gender.FEMALE) gen = "F";
+        else if (gender == Gender.MALE) gen = "M";
 		
 		JSONObject obj = new JSONObject(input);
 		JSONArray arr = obj.getJSONArray("items");
 		for (int i = 0; i < arr.length(); i++) {
 			JSONObject character = (JSONObject)arr.get(i);
 			
-		    System.out.println(""character.get("title") + );
+			String title = character.getString("title");
+			String wikiaPage = wikiaUrl + character.getString("url");
+			String wikiaAbstract = character.getString("abstract");
+			String image = character.get("thumbnail").toString();
+//			if (!title.contains("'s")) {
+				output.append(gen + ",\"" + title + "\"," + wikiaPage + "\",\"" + wikiaAbstract + "\",\"" + image + "\"\n");
+//			}
 		}
 		
-		return "";
+		return output.toString();
 	}
 	
 	public static void main(String[] args) throws IOException {
 		
+		String femaleUrl = "http://harrypotter.wikia.com/api/v1/Articles/List?expand=1&category=Females&limit=2000";
+		String maleUrl = "http://harrypotter.wikia.com/api/v1/Articles/List?expand=1&category=Males&limit=2000";
 		
+		WikiaCharacters wc = new WikiaCharacters();
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("data/wikia.harry.potter.female.character.csv"));
+		bw.write(wc.listOfCharacters("http://harrypotter.wikia.com", femaleUrl, Gender.FEMALE));
+		bw.close();
+		
+		bw = new BufferedWriter(new FileWriter("data/wikia.harry.potter.male.character.csv"));
+		bw.write(wc.listOfCharacters("http://harrypotter.wikia.com", maleUrl, Gender.MALE));
+		bw.close();
 
 		
 	}
